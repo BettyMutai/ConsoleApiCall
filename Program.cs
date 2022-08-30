bbc2527b95ae07cb852ec2e5524999a15be62be6
Betty Mutai <bettymutai00@gmail.com>
2022-08-30 14:38:07 +0300

    Add Restsharp package and create class ApiHelper with static method ApiCall which takes an apiKey

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+
+namespace ApiTest;
+
+class Program
+{
+    static void Main()
+    {
+        Task<string> apiCallTask = ApiHelper.ApiCall("jQtzwDO3kHjG2VsZrpYhRfNdtG9r9vYf");
+        string result = apiCallTask.Result;
+        Console.WriteLine(result);
+    }
+}
+
+class ApiHelper
+{
+    public static async Task<string> ApiCall(string apiKey)
+    {
+        var client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+        var request = new RestRequest($"home.json?api-key={apiKey}", Method.Get);
+        var response = await client.ExecuteAsync(request);
+        return response.Content;
+    }
+}
+
